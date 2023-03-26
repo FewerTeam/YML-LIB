248703f20a1c4958ccd40f529c0a35c029eb5a4a
FewerElk <gabriel@trouilhet.fr>
2023-03-26 18:25:50 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+"""YML python librairie. This librairie allow python developpeurs to open, create and edit .yml files."""
+
+class YMLFile(object):
+    """Class for YML files."""
+    def __init__(self, path, name):
+        """Constructor of this class.
+        Arguments :
+        - path : the path to the file
+        - name : the name of the file"""
+        ...
